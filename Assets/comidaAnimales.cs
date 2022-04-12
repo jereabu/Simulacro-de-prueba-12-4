3969b3ead359bb03e0ac93d7bf96acfd8eed0273
jereabu <jeremiasabulafia@gmail.com>
2022-04-12 08:58:41 -0300

    Fixed PG, PP and G

--- Assets/comidaAnimales.cs
+++ Assets/comidaAnimales.cs
@@ -12,27 +12,25 @@ public class comidaAnimales : MonoBehaviour
             switch (animal){
                 case "G":
                     int comidaGato = dias * 300;
-                    int precioGato = comidaGato * 80;
+                    int precioGato = (comidaGato / 100) * 80;
                     Debug.Log("Para ese periodo se necesitan " + comidaGato + " gramos de alimento");
                     Debug.Log("El costo de la comida va a ser " + precioGato + " pesos");
                     break;
                 case "PP":
                     int comidaPerroP = dias * 400;
-                    int precioPerroP = comidaPerroP * 80;
+                    int precioPerroP = (comidaPerroP / 100) * 80;
                     Debug.Log("Para ese periodo se necesitan " + comidaPerroP + " gramos de alimento");
                     Debug.Log("El costo de la comida va a ser " + precioPerroP + " pesos");
                     break;                
                 case "PG":
                     int comidaPerroG = dias * 700;
-                    int precioPerroG = comidaPerroG * 80;
+                    int precioPerroG = (comidaPerroG / 100) * 80;
                     Debug.Log("Para ese periodo se necesitan " + comidaPerroG + " gramos de alimento");
                     Debug.Log("El costo de la comida va a ser " + precioPerroG + " pesos");
                     break;
                 default:
                     Debug.Log("No ingresaste un animal valido");
                     break;
-
-
             }
         }
         else
